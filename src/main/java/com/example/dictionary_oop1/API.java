cc14cc74cb83235fdcf1b5a7ee5c0391810d0ea8
Minhkwan <minhkwan.it@gmail.com>
2021-11-01 20:54:51 +0700

    Update API

--- src/main/java/com/example/dictionary_oop1/API.java
+++ src/main/java/com/example/dictionary_oop1/API.java
@@ -1,2 +1,34 @@
-package com.example.dictionary_oop1;public class API {
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class API {
+    /**
+     * Translates using Google API.
+     * Source: https://stackoverflow.com/questions/8147284/how-to-use-google-translate-api-in-my-java-application
+     */
+    public static String translate(String langFrom, String langTo, String text) throws IOException {
+        String urlStr = "https://script.google.com/macros/s/AKfycbyaVAdXX09xujMmdY8NrfMN_wHI9rQef4jA9h34Gm0CblTyydfaj_TeWZ8BFdkQNmgB/exec" +
+                "?q=" + URLEncoder.encode(text, StandardCharsets.UTF_8) +
+                "&target=" + langTo +
+                "&source=" + langFrom;
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        return response.toString();
+    }
 }
